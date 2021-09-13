8eb7f42ecfb1ad6ab847f209a3e45067063aadc9
NXZAS8CA <52660198+NXZAS8CA@users.noreply.github.com>
2021-09-13 11:45:09 +0200

    tried to fix marquee in news

--- html/joel/test.php
+++ html/joel/test.php
@@ -1,5 +1,31 @@
-<?php
-$start = strtotime('12:01');
-$end = strtotime('13:16');
-$minutes = ($start - $end) / 60;
-echo "The difference in minutes is $minutes minutes.";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+         
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery.Marquee/1.5.0/jquery.marquee.min.js"></script>
+     
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<script>
+	$(function(){
+  $('.marquee').marquee({
+      duration: 15000,
+      gap: 500,
+      direction: 'left',
+      duplicated: true  
+  });
+});
+</script>
+
+
+</head>
+<body>
+<div class="marquee">Hier kommt ein Text.</div>
+</body>
+</html>
